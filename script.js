151f804f74b9315a2028f8d2574657dbf41eaab7
LeptonFox <143444667+LeptonFox@users.noreply.github.com>
2024-07-30 10:19:19 -0500

    Service worker added to script

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(
+            (registration) => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            },
+            (error) => {
+                console.log('Service Worker registration failed:', error);
+            }
+        );
+    });
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const countryInfo = {
         name: 'USA',
